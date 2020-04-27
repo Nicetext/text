fd334464ffe79a16ccfdfa4c86584d90e40c1675
e <337623361@qq.com>
2020-04-27 23:10:54 +0800

    测试

---
+++ 2-评论案例/src/index.js
@@ -0,0 +1,8 @@
+//1.引入react核心库
+import React,{Component} from 'react'
+//2.引入react-dom库
+import ReactDOM from 'react-dom'
+//3.引入App(所有组件的壳子)
+import App from './App'
+//渲染组件App到root页面显示容器
+ReactDOM.render(<App/>,document.getElementById('root'))
